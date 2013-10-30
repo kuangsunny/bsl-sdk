8de80d0f2278c3e256ddc6054191248b294aff88
Justin Yip <justin.yip@foxmail.com>
2013-10-30 19:01:17 +0800

    add android platform

---
+++ platforms/android/gen/com/foreveross/bsl/sdk/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.foreveross.bsl.sdk;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ platforms/android/src/com/foreveross/bsl/sdk/BSLApplication.java
@@ -0,0 +1,5 @@
+package com.foreveross.bsl.sdk;
+
+public class BSLApplication {
+
+}
